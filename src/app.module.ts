a069a693a82cf7d491d0324ff33e61c5ac805813
Filipi Rafael <filipirafael.123@gmail.com>
2022-03-08 11:50:32 -0300

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksController } from './tasks/tasks.controller';
+import { TasksService } from './tasks/shared/tasks.service';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TasksModule],
+  controllers: [AppController, TasksController],
+  providers: [AppService, TasksService],
 })
 export class AppModule {}
